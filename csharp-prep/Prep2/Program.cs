18297f9605303625b968391ddb1facb893227a8a
Andrew Jayasinghe <55458355+andrewjayasinghe@users.noreply.github.com>
2023-09-13 21:45:11 -0700

    week 1

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your percentage grade? ");
+        string value = Console.ReadLine();
+        int percentage = int.Parse(value);
+
+        string grade = "";
+
+        if (percentage >= 90){
+            grade = "A";
+        }
+
+        else if (percentage>= 80){
+            grade = "B";
+        }
+
+        else if (percentage>= 70){
+        grade = "C";
+        }
+
+        else if (percentage>= 60){
+        grade = "D";
+         }
+
+        else {
+        grade = "F";
+        }
+
+        Console.WriteLine($"Your Letter grade is: {grade}");
+
+        if (percentage >= 70){
+
+        Console.WriteLine("Congratulations! You passed!");
+
+        }
+        else{
+        
+        Console.WriteLine("Good Luck next time!");
+
+        }
+
     }
 }
